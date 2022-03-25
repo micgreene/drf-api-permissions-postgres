3e3ade101403b3968dad4a934880597072ec72d7
micgreene <mgreene11111@yahoo.com>
2022-03-24 18:25:06 -0700

    finished README.md, updated database to run on postgresql, finished unit testing. project complete.

--- drf_permissions_postgresql_project/settings.py
+++ drf_permissions_postgresql_project/settings.py
@@ -75,10 +75,22 @@ WSGI_APPLICATION = 'drf_permissions_postgresql_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+#built-in default database
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
